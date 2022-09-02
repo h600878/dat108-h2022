e8217332e22490fe19197f6779d76acb7581cecb
Lars-Petter Helland <lars-petter.helland@hvl.no>
2022-09-02 07:44:48 +0200

    Til F04 - Animated Balls

---
+++ Animated Balls/src/animated/nothreads/Constants.java
@@ -0,0 +1,13 @@
+package animated.nothreads;
+
+public class Constants {
+	
+	public static final int PANE_SIZE = 500;
+	
+	public static final int BALL_DIAMETER = 30;
+	
+	public static final int MAX_VX_VY = 20;  
+	
+	public static final int SLEEP_TIME = 80;  
+
+}
