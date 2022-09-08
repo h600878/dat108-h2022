42038303828038e508c41cb64667ffe765b3838f
Lars-Petter Helland <lars-petter.helland@hvl.no>
2022-09-08 11:22:14 +0200

    F06 - Utgangspunkt før timen

---
+++ F06 - Last ned til timen/src/no/hvl/lph/dat108/f06/F06b.java
@@ -0,0 +1,28 @@
+package no.hvl.lph.dat108.f06;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class F06b {
+
+	public static void main(String[] args) {
+
+		List<String> listen = Arrays.asList("Hallo", "blabla", "knut", "Per");
+		
+		//Vi gjør resten i timen!
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ F06 - Last ned til timen/src/no/hvl/lph/dat108/f06/F06c.java
@@ -0,0 +1,15 @@
+package no.hvl.lph.dat108.f06;
+
+import java.util.List;
+
+public class F06c {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> listen = List.of(-4, 9, 2, 7, 6, 0);
+		
+		//Vi gjør resten i timen!
+	}
+		
+}
+
