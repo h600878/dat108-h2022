8652b54432db5a52d7a4e4c70a646a714e00ba00
Lars-Petter Helland <lars-petter.helland@hvl.no>
2022-09-12 11:14:59 +0200

    Eksempler til forelesning F07

---
+++ F07 - Last ned til timen/src/no/hvl/dat108/f06/F06a.java
@@ -0,0 +1,90 @@
+package no.hvl.dat108.f06;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class F06a {
+	
+	public static void main(String[] args) {
+		
+		//Sortere liste av heltall.
+		List<Integer> listeAvTall = Arrays.asList(4,2,7,5,9,1);
+		sorter(listeAvTall);
+		System.out.println("Sortert listeAvTall: " + listeAvTall);
+				
+		//Sortere liste av strenger. 
+		//Må bruke comareTo(), generics og typeparameter T extends Comparable<T>. 
+		List<String> listeAvString = Arrays.asList("Aaa", "Xxx", "Bbb", "Kkk");
+		sorter(listeAvString);
+		System.out.println("Sortert listeAvString: " + listeAvString);
+		
+		//Sortere liste av personer på etternavn.
+		//Person implementerer Comparable<Person>.
+		List<Person> listeAvPersoner = Arrays.asList(
+				new Person("Anne", "Persen", 1985),
+				new Person("Per", "Andersen", 1984),
+				new Person("Knut", "Svendsen", 1990));
+		sorter(listeAvPersoner);
+		System.out.println("Sortert listeAvPersoner: " + listeAvPersoner);
+		
+		//Ønsker å sortere på fornavn. Hva gjør vi?
+		//Angir dette via en ekstra parameter som angir hvordan det skal sammenlignes.
+		sorter(listeAvPersoner, new FornavnSammenligner());
+		System.out.println("Sortert listeAvPersoner: " + listeAvPersoner);
+		
+		//Ønsker å sortere på fødselsår. Hva gjør vi?
+		//Samme her, nå som anonym klasse. 
+		sorter(listeAvPersoner, new Sammenligner<Person>() {
+			@Override
+			public int sammenlign(Person a, Person b) {
+				return a.fodselsaar - b.fodselsaar;
+			}
+		});
+		System.out.println("Sortert listeAvPersoner: " + listeAvPersoner);
+	}
+
+
+	//Den "gamle" metoden, nå som et kall til den nye for å slippe duplikatkode
+	private static <T extends Comparable<T>> void sorter(
+			List<T> liste) {
+		
+		sorter(liste, new Sammenligner<T>() {
+			@Override
+			public int sammenlign(T a, T b) {
+				return a.compareTo(b);
+			}
+		});
+	}
+
+	//Den nye metoden med en ekstra Sammenligner-parameter
+	private static <T extends Comparable<T>> void sorter(
+			List<T> liste, Sammenligner<T> s) {
+		
+		for (int i=0; i<liste.size(); i++) {
+			for (int j=1; j<liste.size(); j++) {
+				T a = liste.get(j-1);
+				T b = liste.get(j);
+				
+				if (s.sammenlign(a,b) > 0) {
+					
+					liste.set(j-1, b);
+					liste.set(j, a);
+				}
+			}
+		}
+	}
+}
+
+//-- De nedenfor kunne gjerne vært i egne filer, men ...
+
+interface Sammenligner<T> {
+	public int sammenlign(T a, T b);
+}
+
+class FornavnSammenligner implements Sammenligner<Person> {
+	@Override
+	public int sammenlign(Person a, Person b) {
+		return a.fornavn.compareTo(b.fornavn);
+	}
+}
+
---
+++ F07 - Last ned til timen/src/no/hvl/dat108/f06/Person.java
@@ -0,0 +1,31 @@
+package no.hvl.dat108.f06;
+
+public class Person implements Comparable<Person> {
+	
+	//Har fjernet private for å slippe å bruke gettere. Latskap.
+	String fornavn;
+	String etternavn;
+	int fodselsaar;
+
+	public Person(String fornavn, String etternavn, int fodselsaar) {
+		this.fornavn = fornavn;
+		this.etternavn = etternavn;
+		this.fodselsaar = fodselsaar;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + fornavn + " " + etternavn + ", f. " + fodselsaar + "]";
+	}
+
+	@Override
+	public int compareTo(Person other) {
+		return this.etternavn.compareTo((other.etternavn));
+	}
+}
+
+
+
+
+
+
