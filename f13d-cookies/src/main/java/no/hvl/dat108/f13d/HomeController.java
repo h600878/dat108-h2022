2c6178cdb5685b573d0555fec72878f6b5502aeb
Martin Berg Alstad <600878@stud.hvl.no>
2022-10-08 19:37:50 +0200

    Video 4

--- f13d-cookies/src/main/java/no/hvl/dat108/f13d/HomeController.java
+++ f13d-cookies/src/main/java/no/hvl/dat108/f13d/HomeController.java
@@ -16,21 +16,31 @@ public class HomeController {
 
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy_HH:mm:ss");
 
+    /**
+     * Viser en enkel side med en melding første gang man er innom. Hvis man besøker innen 30 sekunder får man en annen melding.
+     * @param sisteBesok En cookie som vi skal lete etter, ved navn "sisteBesok" og den er ikke nødvendig
+     * @param model Model for .jsp
+     * @param response Header
+     * @return Et view til homeView
+     */
     @GetMapping("/home")
     public String home(
             @CookieValue(name = "sisteBesok", required = false) String sisteBesok,
             Model model, HttpServletResponse response) {
 
+        // Hvis den ikke eksisterer
         if (sisteBesok == null) { // evt. kan man bruke Optional
             model.addAttribute("melding", "Hei. Velkommen første gang til oss.");
         }
+        // Hvis den allerede eksisterer
         else {
             model.addAttribute("melding", "Hei. Ser du var sist inne " + sisteBesok);
         }
 
+        // Oppretter en ny cookie, hvor "sisteBesok" er navnet på cookien
         Cookie c = new Cookie("sisteBesok", LocalDateTime.now().format(formatter));
-        c.setMaxAge(10);
-        response.addCookie(c);
+        c.setMaxAge(10); // Setter maks levetid på cookien i sekunder, en negativ verdi gir oss en session-cookie, 0 sletter
+        response.addCookie(c); // Legger til cookie
 
         return "homeView";
     }
