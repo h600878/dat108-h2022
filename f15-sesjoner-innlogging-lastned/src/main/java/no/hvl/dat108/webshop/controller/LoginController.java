0a023c9ceec14e07198598144fb63614ce7aa3e3
Lars-Petter Helland <lars-petter.helland@hvl.no>
2022-10-10 12:18:39 +0200

    Eksempel til forelesning F15

---
+++ f15-sesjoner-innlogging-lastned/src/main/java/no/hvl/dat108/webshop/controller/LoginController.java
@@ -0,0 +1,34 @@
+package no.hvl.dat108.webshop.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	private String INVALID_USERNAME_MESSAGE = "Manglende eller ugyldig brukernavn";
+	
+	/* 
+	 * GET /login er forespørselen for å hente login-skjema.
+	 */
+	@GetMapping
+    public String hentLoginSkjema() {
+		return "loginView";
+    }
+
+	/* 
+	 * POST /login er forespørselen for å logge inn.
+	 */
+	@PostMapping
+    public String provAaLoggeInn(@RequestParam String username,
+    		HttpServletRequest request,	RedirectAttributes ra) {
+		return "redirect:" + "webshop";
+    }
+}
---
+++ f15-sesjoner-innlogging-lastned/src/main/java/no/hvl/dat108/webshop/controller/WebshopController.java
@@ -0,0 +1,37 @@
+package no.hvl.dat108.webshop.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/webshop")
+public class WebshopController {
+	
+	private String REQUIRES_LOGIN_MESSAGE = "Forespørselen din krever at du er innlogget";
+
+	/* 
+	 * GET /webshop er forespørselen for å vise webshop-siden.
+	 */
+	@GetMapping
+    public String visWebshoppen(HttpSession session, RedirectAttributes ra) {
+		return "webshopView";
+    }
+
+	/* 
+	 * POST /webshop er forespørselen for å handle en/flere varer.
+	 */
+	@PostMapping
+    public String leggVarerIHandlekurv(
+    		@RequestParam(name="vare", required=false) List<String> varer,
+    		HttpSession session, RedirectAttributes ra) {
+		return "redirect:" + "webshop";
+    }
+}
