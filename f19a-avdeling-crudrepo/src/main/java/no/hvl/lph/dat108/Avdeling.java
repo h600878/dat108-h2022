397265299e0ff031d8bc00f1134b810a2e06b4ca
Lars-Petter Helland <lars-petter.helland@hvl.no>
2022-10-24 11:52:37 +0200

    F19-eksempler om Spring Data JPA

---
+++ f19a-avdeling-crudrepo/src/main/java/no/hvl/lph/dat108/Avdeling.java
@@ -0,0 +1,42 @@
+package no.hvl.lph.dat108;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "ansatt_avdeling")
+public class Avdeling {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Integer id ;
+	
+	private String navn;
+	
+	public Avdeling() {}
+	
+	@Override
+	public String toString() {
+		return String.format("Avdeling: id=%d, navn=%s", id, navn);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNavn() {
+		return navn;
+	}
+
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+
+}
