1e09fe4a0dca2b43b4539385002c5eeb1d08ca06
Martin Berg Alstad <600878@stud.hvl.no>
2022-11-24 20:11:32 +0100

    Formatering

--- F03 - wait notify/src/waitnotify/Main.java
+++ F03 - wait notify/src/waitnotify/Main.java
@@ -2,39 +2,40 @@ package waitnotify;
 
 public class Main {
 
-	private static String melding;
+    private static String melding;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Object lock = new Object();
+        Object lock = new Object();
 
-		Thread printlnTraad = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				synchronized (lock) {
-					while (melding == null) {
-						try {
-							lock.wait();
-						} catch (InterruptedException e) {
-						}
-					}
-				}
-				System.out.println(melding);
-			}
-		});
+        Thread printlnTraad = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock) { // Må synkronisere mot tråden som låser
+                    while (melding == null) {
+                        try {
+                            lock.wait();
+                        }
+                        catch (InterruptedException ignored) {
+                        }
+                    }
+                }
+                System.out.println(melding);
+            }
+        });
 
-		Thread giVerdiTraad = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				synchronized(lock) {
-					melding = "Hallo";
-					lock.notifyAll();
-				}
-			}
-		});
+        Thread giVerdiTraad = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock) {
+                    melding = "Hallo";
+                    lock.notifyAll();
+                }
+            }
+        });
 
-		printlnTraad.start();
-		giVerdiTraad.start();
-	}
+        printlnTraad.start();
+        giVerdiTraad.start();
+    }
 
 }
