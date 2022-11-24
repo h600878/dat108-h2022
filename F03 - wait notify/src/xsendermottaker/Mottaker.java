5bdf45555559c0e7f6654c3637a44862116b7705
Martin Berg Alstad <600878@stud.hvl.no>
2022-11-24 20:12:52 +0100

    Formatering

--- F03 - wait notify/src/xsendermottaker/Mottaker.java
+++ F03 - wait notify/src/xsendermottaker/Mottaker.java
@@ -1,34 +1,37 @@
 package xsendermottaker;
 
 public class Mottaker extends Thread {
-	
-	private Melding melding;
 
-	public Mottaker(Melding melding) {
-		this.melding = melding;
-	}
-	
-	@Override
-	public void run() {
-		
-		while(true) {
-			try {
-				sleep(10_000);
-			} catch (InterruptedException e) {
-			}
-			
-			synchronized(melding) {
-				while(melding.erMeldingLest()) {
-					try {
-						melding.wait();
-					} catch (InterruptedException e) {
-					}
-				}
-				String mottattTekst = melding.getTekst();
-				melding.notifyAll();
-				System.out.println("Mottaker har mottatt melding " + mottattTekst);
-			}
-		}
-	}
+    private final Melding melding;
+
+    public Mottaker(Melding melding) {
+        this.melding = melding;
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            try {
+                //noinspection BusyWait
+                sleep(10_000);
+            }
+            catch (InterruptedException ignored) {
+            }
+
+            synchronized (melding) {
+                while (melding.erMeldingLest()) {
+                    try {
+                        melding.wait();
+                    }
+                    catch (InterruptedException ignored) {
+                    }
+                }
+                String mottattTekst = melding.getTekst();
+                melding.notifyAll();
+                System.out.println("Mottaker har mottatt melding " + mottattTekst);
+            }
+        }
+    }
 
 }
